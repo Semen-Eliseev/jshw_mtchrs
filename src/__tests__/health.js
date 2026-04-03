8a427d7a8cf695e3658864ff33f352ab74d61514
ruser <eliseevsemnik@yandex.ru>
2026-04-03 09:36:48 -0800

    Перенос решения, настройка eslint

---
+++ src/__tests__/health.js
@@ -0,0 +1,25 @@
+import { sortedHealth, sortingHealth } from "../health.js";
+
+test('sorts by health descending', () => { 
+       const input = [ 
+             { name: 'мечник', health: 10 },
+             { name: 'маг', health: 100 }, 
+             { name: 'лучник', health: 80 },
+        ];
+    expect(sortingHealth(input)).toEqual([ 
+             { name: 'маг', health: 100 }, 
+             { name: 'лучник', health: 80 }, 
+             { name: 'мечник', health: 10 }
+   ]);  
+});
+
+test('sorts by health descending', () => { 
+       const input = [ 
+             { name: 'мечник', health: 10 },
+             
+        ];
+    expect(sortingHealth(input)).toEqual([ 
+             
+             { name: 'мечник', health: 10 }
+   ]);  
+});
---
+++ src/health.js
@@ -0,0 +1,9 @@
+export function sortingHealth(items) {
+    if (items.length >1) {
+       const sorted = items.sort((a, b) => b.health - a.health);
+       return sorted;
+    }else {
+        return items;
+    }
+
+}; 
