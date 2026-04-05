230567a4dd037be81ef04701b6a288c315482734
SteamOS User <eliseevsemnik@yandex.ru>
2026-04-05 14:06:55 +0300

    исправлен eslint.config.mjs и ошибки которые показал npm run lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-      },
-    },
+export default [   
+  {    
+    languageOptions: 
+    {      
+      parserOptions: {        
+        ecmaVersion: 2024,        
+        sourceType: "module",        
+        ecmaFeatures: { jsx: true }      
+      },      
+    },     
   },
-]);
+  {    
+    rules: {      
+      "indent": ["error", 4, {        
+        "SwitchCase": 1,        
+        "VariableDeclarator": 1,        
+        "outerIIFEBody": 1,        
+        "MemberExpression": 1,        
+        "FunctionDeclaration": { "parameters": "first" },        
+        "FunctionExpression": { "parameters": "first" },       
+        "CallExpression": { "arguments": "first" },        
+        "ignoredNodes": ["TemplateLiteral *"],        
+        "flatTernaryExpressions": false      
+      }],      
+      "linebreak-style": ["error", "unix"],      
+      "quotes": ["error", "single", { "avoidEscape": true }],      
+      "semi": ["error", "always"]    
+    }  
+  }];
--- src/__tests__/health.js
+++ src/__tests__/health.js
@@ -1,25 +1,25 @@
-import { sortingHealth } from "../health.js";
+import { sortingHealth } from '../health.js';
 
 test('sorts by health descending', () => { 
-       const input = [ 
-             { name: 'мечник', health: 10 },
-             { name: 'маг', health: 100 }, 
-             { name: 'лучник', health: 80 },
-        ];
+    const input = [ 
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 }, 
+        { name: 'лучник', health: 80 },
+    ];
     expect(sortingHealth(input)).toEqual([ 
-             { name: 'маг', health: 100 }, 
-             { name: 'лучник', health: 80 }, 
-             { name: 'мечник', health: 10 }
-   ]);  
+        { name: 'маг', health: 100 }, 
+        { name: 'лучник', health: 80 }, 
+        { name: 'мечник', health: 10 }
+    ]);  
 });
 
 test('sorts by health descending', () => { 
-       const input = [ 
-             { name: 'мечник', health: 10 },
+    const input = [ 
+        { name: 'мечник', health: 10 },
              
-        ];
+    ];
     expect(sortingHealth(input)).toEqual([ 
              
-             { name: 'мечник', health: 10 }
-   ]);  
+        { name: 'мечник', health: 10 }
+    ]);  
 });
